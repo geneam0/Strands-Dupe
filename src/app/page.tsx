b1805d8013158e7849bb1c9e39f47d90f935508d
Gene Lam <gene.lam@macaulay.cuny.edu>
2024-05-11 14:24:23 -0400

    fixed appostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import { api } from "~/trpc/server";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-[#C0DDD9] text-black">
       <div className="flex flex-col items-center">
-        <img
+        <Image
           src="https://www.nytimes.com/games/prototype/dist/strands100px.ea3b90d2.svg"
           alt="Strands Logo"
           className="mb-8"
@@ -15,7 +16,7 @@ export default function Home() {
         <h1 className="text-4xl font-bold">Strands</h1>
         <p className="mb-4 text-xl">Budhi Birthday Edition</p>
         <p className="mb-6 w-[304px] text-center text-2xl">
-          Find hidden words and uncover the day's theme
+          Find hidden words and uncover the day&apos;s theme
         </p>
         <button className="mb-4 h-[48px] w-[150px] rounded-full bg-black px-6 py-2 text-white">
           Play
